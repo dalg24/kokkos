03e2df897e24378aea051a7f89a56bcf40486271
Damien L-G <dalg24@gmail.com>
2019-04-22 11:12:34 -0400

    Add compose file and build script

---
+++ docker/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+EXTRA_ARGS=("$@")
+
+ARGS=(
+  -D CMAKE_CXX_COMPILER=nvcc_wrapper
+  -D CMAKE_FIND_DEBUG_MODE=ON
+  -D CMAKE_PREFIX_PATH=/opt/kokkos
+  -D CMAKE_MODULE_PATH=/scratch/source/cmake
+#  -D CMAKE_PREFIX_PATH="/opt/kokkos;/opt/kokkos/lib/CMake"
+)
+
+rm -rf CMake*
+
+cmake "${ARGS[@]}" "${EXTRA_ARGS[@]}" /scratch/source
+
+make
