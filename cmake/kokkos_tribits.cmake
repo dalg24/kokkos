11f8cf245561d93d62c5b0f397ee7db059b5ba62
Damien L-G <dalg24@gmail.com>
2026-03-06 07:11:50 -0500

    Try to fix Windows builds
    Signed-off-by: Damien L-G <dalg24@gmail.com>
    Signed-off-by: Daniel Arndt <arndtd@ornl.gov>

--- cmake/kokkos_tribits.cmake
+++ cmake/kokkos_tribits.cmake
@@ -16,6 +16,8 @@ if(Kokkos_ENABLE_TESTS OR Kokkos_INSTALL_TESTING)
     list(APPEND CMAKE_MESSAGE_INDENT "[googletest] ")
     set(INSTALL_GTEST OFF)
     FetchContent_Declare(googletest SOURCE_DIR ${Kokkos_SOURCE_DIR}/tpls/googletest-1.14.0)
+    # For Windows: Prevent overriding the parent project's compiler/linker settings
+    set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
     FetchContent_MakeAvailable(googletest)
     list(POP_BACK CMAKE_MESSAGE_INDENT)
 
