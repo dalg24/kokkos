64c800271a38127ca708dc9cb744cab9977640fe
Damien L-G <dalg24@gmail.com>
2026-06-01 13:26:37 -0400

    Desul atomics: Let atomic_{load,store} be a primitive that must be implemented by backends
    Signed-off-by: Damien L-G <dalg24@gmail.com>

---
+++ tpls/desul/include/desul/atomics/Load_And_Store_CUDA.hpp
@@ -0,0 +1,12 @@
+/*
+Copyright (c) 2019, Lawrence Livermore National Security, LLC
+and DESUL project contributors. See the COPYRIGHT file for details.
+Source: https://github.com/desul/desul
+
+SPDX-License-Identifier: (BSD-3-Clause)
+*/
+
+#ifndef DESUL_ATOMICS_LOAD_AND_STORE_CUDA_HPP_
+#define DESUL_ATOMICS_LOAD_AND_STORE_CUDA_HPP_
+
+#endif
---
+++ tpls/desul/include/desul/atomics/Load_And_Store_SYCL.hpp
@@ -0,0 +1,97 @@
+/*
+Copyright (c) 2019, Lawrence Livermore National Security, LLC
+and DESUL project contributors. See the COPYRIGHT file for details.
+Source: https://github.com/desul/desul
+
+SPDX-License-Identifier: (BSD-3-Clause)
+*/
+
+#ifndef DESUL_ATOMICS_LOAD_AND_STORE_SYCL_HPP_
+#define DESUL_ATOMICS_LOAD_AND_STORE_SYCL_HPP_
+
+#include <desul/atomics/Adapt_SYCL.hpp>
+#include <desul/atomics/Compare_Exchange_SYCL.hpp>
+
+namespace desul {
+namespace Impl {
+
+template <class T, class MemoryOrder, class MemoryScope>
+std::enable_if_t<device_atomic_always_lock_free<T>, void> device_atomic_store(
+    T* ptr, T val, MemoryOrder, MemoryScope) {
+  sycl_atomic_ref<T, MemoryOrder, MemoryScope> ref(*ptr);
+  ref.store(val);
+}
+
+template <class T, class MemoryOrder, class MemoryScope>
+std::enable_if_t<device_atomic_always_lock_free<T>, T> device_atomic_load(T* ptr,
+                                                                          MemoryOrder,
+                                                                          MemoryScope) {
+  sycl_atomic_ref<T, MemoryOrder, MemoryScope> ref(*ptr);
+  return ref.load();
+}
+
+template <class T, class MemoryOrder, class MemoryScope>
+std::enable_if_t<!device_atomic_always_lock_free<T>, void> device_atomic_store(
+    T* ptr, T val, MemoryOrder, MemoryScope scope) {
+  // This is a way to avoid deadlock in a subgroup
+  int done = 0;
+#if defined(__INTEL_LLVM_COMPILER) && __INTEL_LLVM_COMPILER >= 20250000
+  auto sg = sycl::ext::oneapi::this_work_item::get_sub_group();
+#else
+  auto sg = sycl::ext::oneapi::experimental::this_sub_group();
+#endif
+  using sycl::ext::oneapi::group_ballot;
+  using sycl::ext::oneapi::sub_group_mask;
+  sub_group_mask active = group_ballot(sg, 1);
+  sub_group_mask done_active = group_ballot(sg, 0);
+  while (active != done_active) {
+    if (!done) {
+      if (lock_address_sycl((void*)dest, scope)) {
+        if (std::is_same<MemoryOrder, MemoryOrderSeqCst>::value)
+          atomic_thread_fence(MemoryOrderRelease(), scope);
+        atomic_thread_fence(MemoryOrderAcquire(), scope);
+        *ptr = val;
+        unlock_address_sycl((void*)dest, scope);
+        done = 1;
+      }
+    }
+    done_active = group_ballot(sg, done);
+  }
+}
+
+template <class T, class MemoryOrder, class MemoryScope>
+std::enable_if_t<!device_atomic_always_lock_free<T>, T> device_atomic_load(
+    T* ptr, MemoryOrder, MemoryScope scope) {
+  // This is a way to avoid deadlock in a subgroup
+  T ret;
+  int done = 0;
+#if defined(__INTEL_LLVM_COMPILER) && __INTEL_LLVM_COMPILER >= 20250000
+  auto sg = sycl::ext::oneapi::this_work_item::get_sub_group();
+#else
+  auto sg = sycl::ext::oneapi::experimental::this_sub_group();
+#endif
+  using sycl::ext::oneapi::group_ballot;
+  using sycl::ext::oneapi::sub_group_mask;
+  sub_group_mask active = group_ballot(sg, 1);
+  sub_group_mask done_active = group_ballot(sg, 0);
+  while (active != done_active) {
+    if (!done) {
+      if (lock_address_sycl((void*)dest, scope)) {
+        if (std::is_same<MemoryOrder, MemoryOrderSeqCst>::value)
+          atomic_thread_fence(MemoryOrderRelease(), scope);
+        atomic_thread_fence(MemoryOrderAcquire(), scope);
+        ret = *ptr;
+        unlock_address_sycl((void*)dest, scope);
+        done = 1;
+      }
+    }
+    done_active = group_ballot(sg, done);
+  }
+  return ret;
+}
+
+}  // namespace Impl
+}  // namespace desul
+
+#endif
+
