3e7e866da39fa05269f119f6f7c4b8b6697d9bc1
Dong Hun Lee <donlee@sandia.gov>
2026-06-18 14:51:21 -0600

    Applying suggtestions from review
    Signed-off-by: Dong Hun Lee <donlee@sandia.gov>

--- core/unit_test/TestFunctionAnnotationMacros.cpp
+++ core/unit_test/TestFunctionAnnotationMacros.cpp
@@ -19,11 +19,11 @@ static_assert(false, "KOKKOS_FORCEINLINE_FUNCTION macro is not defined!");
 static_assert(false, "KOKKOS_RELOCATABLE_FUNCTION macro is not defined!");
 #endif
 
-#if !defined(KOKKOS_INLINE_FUNCTION_DELETED)
+#ifndef KOKKOS_INLINE_FUNCTION_DELETED
 static_assert(false, "KOKKOS_INLINE_FUNCTION_DELETED macro is not defined!");
 #endif
 
-#if !defined(KOKKOS_DEFAULTED_FUNCTION)
+#ifndef KOKKOS_DEFAULTED_FUNCTION
 static_assert(false, "KOKKOS_DEFAULTED_FUNCTION macro is not defined!");
 #endif
 
@@ -97,27 +97,27 @@ struct AnnotationExamples {
   KOKKOS_INLINE_FUNCTION_DELETED AnnotationExamples(double) = delete;
 
   KOKKOS_FUNCTION constexpr int kokkos_function(int x) const {
-    return m_value + x;
+    return m_value * x;
   }
 
   KOKKOS_INLINE_FUNCTION constexpr int kokkos_inline_function(int x) const {
-    return m_value - x;
+    return m_value * (x << 1);
   }
 
   KOKKOS_FORCEINLINE_FUNCTION constexpr int kokkos_forceinline_function(
       int x) const {
-    return m_value * x;
+    return m_value * (x << 2);
   }
 
   KOKKOS_INLINE_FUNCTION constexpr int use_class_lambda(int x) const {
-    auto lambda = KOKKOS_CLASS_LAMBDA(int y) { return m_value + y; };
+    auto lambda = KOKKOS_CLASS_LAMBDA(int y) { return m_value * (y << 3); };
     return lambda(x);
   }
 
   KOKKOS_FORCEINLINE_FUNCTION constexpr int use_class_forceinline_lambda(
       int x) const {
     auto lambda = KOKKOS_CLASS_FORCEINLINE_LAMBDA(int y) {
-      return m_value - y;
+      return m_value * (y << 4);
     };
     return lambda(x);
   }
@@ -133,39 +133,39 @@ KOKKOS_DEDUCTION_GUIDE DeductionGuideExample(Value)
     -> DeductionGuideExample<Value>;
 
 KOKKOS_RELOCATABLE_FUNCTION constexpr int relocatable_plus_one(int value) {
-  return value + 1;
+  return value << 5;
 }
 
 KOKKOS_INLINE_FUNCTION constexpr int use_lambda_annotation(int x) {
-  auto lambda = KOKKOS_LAMBDA(int y) { return y + 2; };
+  auto lambda = KOKKOS_LAMBDA(int y) { return y << 6; };
   return lambda(x);
 }
 
 KOKKOS_FORCEINLINE_FUNCTION constexpr int use_forceinline_lambda_annotation(
     int x) {
-  auto lambda = KOKKOS_FORCEINLINE_LAMBDA(int y) { return y * 2; };
+  auto lambda = KOKKOS_FORCEINLINE_LAMBDA(int y) { return y << 7; };
   return lambda(x);
 }
 
 KOKKOS_FUNCTION constexpr int use_all_function_annotations() {
   AnnotationExamples example{};
-  example.m_value = 4;
+  example.m_value = 1;
 
   int result = 0;
-  result += example.kokkos_function(3);
-  result += example.kokkos_inline_function(1);
-  result += example.kokkos_forceinline_function(2);
-  result += example.use_class_lambda(5);
-  result += example.use_class_forceinline_lambda(2);
-  result += relocatable_plus_one(7);
-  result += use_lambda_annotation(8);
-  result += use_forceinline_lambda_annotation(6);
+  result |= example.kokkos_function(1);
+  result |= example.kokkos_inline_function(1);
+  result |= example.kokkos_forceinline_function(1);
+  result |= example.use_class_lambda(1);
+  result |= example.use_class_forceinline_lambda(1);
+  result |= relocatable_plus_one(1);
+  result |= use_lambda_annotation(1);
+  result |= use_forceinline_lambda_annotation(1);
 
-  auto deduced = DeductionGuideExample{9};
-  result += deduced.m_value;
+  auto deduced = DeductionGuideExample{1 << 8};
+  result |= deduced.m_value;
   return result;
 }
 
-static_assert(use_all_function_annotations() == 68);
+static_assert(use_all_function_annotations() == (1 << 9) - 1);
 
 }  // namespace
