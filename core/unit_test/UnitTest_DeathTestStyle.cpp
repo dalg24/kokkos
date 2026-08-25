5d276c1f6cea00b3aa0fdab47fd3635244315931
Damien L-G <dalg24@gmail.com>
2026-08-25 17:12:57 -0600

    Set flag before init
    Signed-off-by: Damien L-G <dalg24@gmail.com>

--- core/unit_test/UnitTest_DeathTestStyle.cpp
+++ core/unit_test/UnitTest_DeathTestStyle.cpp
@@ -6,11 +6,11 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {  // NOLINT(bugprone-exception-escape)
-  ::testing::InitGoogleTest(&argc, argv);
   // Proposed in https://github.com/kokkos/kokkos/pull/9469
   if (!std::getenv("GTEST_DEATH_TEST_STYLE"))
     ::testing::FLAGS_gtest_death_test_style = "threadsafe";
 
+  ::testing::InitGoogleTest(&argc, argv);
   std::cout << GTEST_FLAG_GET(death_test_style) << "\n";
   return 0;
 }
