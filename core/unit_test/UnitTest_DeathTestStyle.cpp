7c06d32702734c20c8859c75d362ef668e67c0e6
Damien L-G <dalg24@gmail.com>
2026-08-25 16:12:03 -0600

    Show that #9469 breaks GTest's initialization default behavior and takes control away from the user
    Signed-off-by: Damien L-G <dalg24@gmail.com>

--- core/unit_test/UnitTest_DeathTestStyle.cpp
+++ core/unit_test/UnitTest_DeathTestStyle.cpp
@@ -7,6 +7,10 @@
 
 int main(int argc, char* argv[]) {  // NOLINT(bugprone-exception-escape)
   ::testing::InitGoogleTest(&argc, argv);
+  // Proposed in https://github.com/kokkos/kokkos/pull/9469
+  if (!std::getenv("GTEST_DEATH_TEST_STYLE"))
+    ::testing::FLAGS_gtest_death_test_style = "threadsafe";
+
   std::cout << GTEST_FLAG_GET(death_test_style) << "\n";
   return 0;
 }
